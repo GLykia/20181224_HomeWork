159a0ef80d3f9bcc4e8cccc4e6f6851506413270
Emre Uzun <emreuzun2014@gmail.com>
2018-12-24 23:59:43 +0300

    Add project files.

---
+++ Asli-Donmez/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asli_Donmez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baslangic:
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Kaç öğrenci istiyorsunuz ? ");
+            int ogrenciSayisi = 0;
+            if (int.TryParse(Console.ReadLine(), out ogrenciSayisi))
+            {
+                Console.WriteLine("Sayısal Değer Girdiniz");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Düzgün değer gir");
+                Console.ResetColor();
+                goto Baslangic;
+            }
+
+            string[,] ogrenciler = new string[ogrenciSayisi, 3];
+            int[,] MatematikNotlari = new int[ogrenciSayisi, 2];
+            int[,] KimyaNotlari = new int[ogrenciSayisi, 2];
+
+            for (int i = 0; i < ogrenciler.GetLength(0); i++)
+            {
+                Console.WriteLine("{0} öğrencinin ismini giriniz", i + 1);
+                ogrenciler[i, 0] = Console.ReadLine();
+                for (int j = 0; j < KimyaNotlari.GetLength(1); j++)
+                {
+                    Console.WriteLine("{0} adlı öğrencinin {1} kimya notunu giriniz:", ogrenciler[i, 0], j+1);
+                    KimyaNotlari[i, j] = int.Parse(Console.ReadLine());
+                }
+                for (int k = 0; k < MatematikNotlari.GetLength(1); k++)
+                {
+                    Console.WriteLine("{0} adlı öğrencinin {1} MAT notunu giriniz:", ogrenciler[i, 0],k+1);
+                    MatematikNotlari[i, k] = int.Parse(Console.ReadLine());
+                }
+                ogrenciler[i, 1] = ((double)(KimyaNotlari[i, 0] + KimyaNotlari[i, 1]) / 2).ToString();
+                ogrenciler[i, 2] = ((double)(MatematikNotlari[i, 0] + MatematikNotlari[i, 1]) / 2).ToString();
+            }
+            Console.Clear();
+            for (int i = 0; i < ogrenciler.GetLength(0); i++)
+            {
+                Console.WriteLine("{0} adlı ogrencinin Kimya not ortalamsı: {1}, Matematik not ortalaması: {2}", ogrenciler[i, 0], ogrenciler[i, 1],ogrenciler[i,2]);
+            }
+
+            Console.Write("Aranacak öğrenci adı: ");
+            string aranan = Console.ReadLine().ToLower();
+
+            for (int m = 0; m < ogrenciler.GetLength(0);)
+            {
+                if ((ogrenciler.GetLength(0) != m) && (aranan != ogrenciler[m, 0]))
+                {
+                    m++;
+                }
+                else
+                {
+                    Console.WriteLine("Öğrenci adı: {0}\nMatematik Ort: {1}\nKimya Ort: {2}",
+                        ogrenciler[m, 0], ogrenciler[m, 2], ogrenciler[m, 1]);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ boyutludiziler/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boyutludiziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baslangic:
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Kaç öğrenci istiyorsunuz ? ");
+            int ogrenciSayisi = 0;
+            if (int.TryParse(Console.ReadLine(), out ogrenciSayisi))
+            {
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("La düzgün girsene kaç kişiyseniz...");
+                Console.ResetColor();
+                goto Baslangic;
+            }
+            Console.ResetColor();
+            string ogrenciAdi = "";
+            string[,] ogrenciler = new string[ogrenciSayisi,3];
+            double[,] MatematikNotlari = new double[ogrenciSayisi, 2];
+            double[,] KimyaNotlari = new double[ogrenciSayisi, 2];
+            
+
+            for (int i = 0; i < ogrenciler.GetLength(0); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("{0}. öğrencinin adını giriniz = ", i + 1);
+                ogrenciAdi = Console.ReadLine().ToString().ToUpper();
+                ogrenciler[i, 0] = ogrenciAdi;
+                for (int j = 0; j < MatematikNotlari.GetLength(1); j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("\"{0}\" adlı öğrencinin {1}. Matematik notunu giriniz = ", ogrenciler[i,0], j+1);
+                    MatematikNotlari[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+                for (int k = 0; k < KimyaNotlari.GetLength(1); k++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("\"{0}\" adlı öğrencinin {1}. Kimya notunu giriniz = ", ogrenciler[i, 0], k+1);
+                    KimyaNotlari[i, k] = Convert.ToDouble(Console.ReadLine());
+                }
+                ogrenciler[i, 1] = ((MatematikNotlari[i, 0] + MatematikNotlari[i, 1])/2).ToString();
+                ogrenciler[i, 2] = ((KimyaNotlari[i, 0] + KimyaNotlari[i, 1]) / 2).ToString();
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Öğrenciler Ve Ortalamaları Listesi");
+            Console.WriteLine("*******************************************");
+
+            Console.ResetColor();
+            for (int i = 0; i < ogrenciler.GetLength(0); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("{0} adlı öğrencinin Matematik 1.Notu: {1} 2.Notu : {2}", ogrenciler[i, 0], MatematikNotlari[i, 0], MatematikNotlari[i, 1]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} adlı öğrencinin Kimya 1. Notu: {1} 2. Notu : {2}", ogrenciler[i, 0], KimyaNotlari[i, 0], KimyaNotlari[i, 1]);
+                Console.ResetColor();
+                Console.WriteLine("{0} adlı öğrencinin Matematik Not Ort: {1} Kimya Not Ortalaması: {2} ",ogrenciler[i,0],ogrenciler[i,1],ogrenciler[i,2]);
+                Console.WriteLine("*******************************************");
+            }
+            searchagain:
+            Console.WriteLine("Aranan kişinin ismini giriniz");
+            string aranankisi = Console.ReadLine();
+            int tut = 0;
+            for (int i = 0; i < ogrenciler.GetLength(0); i++)
+            {
+                if (aranankisi.ToUpper()==ogrenciler[i,0].ToUpper())
+                {
+                    Console.WriteLine("{0} adlı öğrencinin Matematik 1.Notu: {1} 2.Notu : {2}", ogrenciler[i, 0], MatematikNotlari[i, 0], MatematikNotlari[i, 1]);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("{0} adlı öğrencinin Kimya 1. Notu: {1} 2. Notu : {2}", ogrenciler[i, 0], KimyaNotlari[i, 0], KimyaNotlari[i, 1]);
+                    Console.ResetColor();
+                    Console.WriteLine("{0} adlı öğrencinin Matematik Not Ort: {1} Kimya Not Ortalaması: {2} ", ogrenciler[i, 0], ogrenciler[i, 1], ogrenciler[i, 2]);
+                }
+                else
+                {
+                    Console.WriteLine("Aradığın kişi bulunamadı");
+                    tut++;
+                }
+            }
+            if (tut==ogrenciler.GetLength(0))
+            {
+                goto searchagain;
+            }
+        }
+    }
+}
